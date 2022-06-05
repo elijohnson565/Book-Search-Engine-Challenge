8d93fdf0864ec412daedcf205fab84ad8df74d02
Elijah Johnson <elijohnson565@gmail.com>
2022-06-04 18:56:05 -0700

    finished util js files

---
+++ client/utils/localStorage.js
@@ -0,0 +1,34 @@
+//get the saved books
+export const getSavedBookIds = () => {
+  const savedBookIds = localStorage.getItem('saved_books')
+    ? JSON.parse(localStorage.getItem('saved_books'))
+    : [];
+
+  return savedBookIds;
+};
+
+//save books by id
+export const saveBookIds = (bookIdArr) => {
+  if (bookIdArr.length) {
+    localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
+  } else {
+    localStorage.removeItem('saved_books');
+  }
+};
+
+//remove books by id
+export const removeBookId = (bookId) => {
+  const savedBookIds = localStorage.getItem('saved_books')
+    ? JSON.parse(localStorage.getItem('saved_books'))
+    : null;
+
+  if (!savedBookIds) {
+    return false;
+  }
+
+  //save changed books
+  const changedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
+  localStorage.setItem('saved_books', JSON.stringify(changedBookIds));
+
+  return true;
+};
